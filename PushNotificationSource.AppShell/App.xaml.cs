6e39d61921c5876e6ac28a4ddd517172da07395d
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-04-16 22:05:35 +0200

    Initial commit

---
+++ PushNotificationSource.AppShell/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace PushNotificationSource.AppShell
+{
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var mainPage = (MainPage) Window.Current.Content;
+
+            if (mainPage == null)
+            {
+                mainPage = new MainPage();
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                }
+
+                Window.Current.Content = mainPage;
+            }
+
+            if (e.PrelaunchActivated) return;
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ PushNotificationSource.AppShell/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using WebPush;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace PushNotificationSource.AppShell
+{
+    public sealed partial class MainPage
+    {
+        private static readonly WebPushClient WebPushClient = new WebPushClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            PublicKeyTextBox.Text = "BBrspYRJxJQYsqzVp8FuRPqOQ9L6F5vRbIMQu9CeiYGEP68aEVem5b6e1QK2BQYgNIKh4nORAyjKYEZAE7D5SAA";
+            PrivateKeyTextBox.Text = "HByTkaPfAXU1TA5eIVqQfLKYAXwRYo388Lx63vXI5OA";
+        }
+
+        private async void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            MessageTextBox.Focus(FocusState.Programmatic);
+            ButtonPushToSelf.IsEnabled = false;
+
+            try
+            {
+                await SendAsync(SubscriptionTextBox.Text, MessageTextBox.Text,
+                    PublicKeyTextBox.Text, PrivateKeyTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                await new MessageDialog(ex.ToString()).ShowAsync();
+            }
+
+            ButtonPushToSelf.IsEnabled = true;
+        }
+
+        public class Subscription
+        {
+            // ReSharper disable once UnusedAutoPropertyAccessor.Global
+            public string ChannelUri { get; set; }
+            // ReSharper disable once UnusedAutoPropertyAccessor.Global
+            public SubscriptionKeys Keys { get; set; }
+        }
+
+        // ReSharper disable once ClassNeverInstantiated.Global
+        public class SubscriptionKeys
+        {
+            // ReSharper disable once UnusedAutoPropertyAccessor.Global
+            public string P256Dh { get; set; }
+            // ReSharper disable once UnusedAutoPropertyAccessor.Global
+            public string Auth { get; set; }
+        }
+
+        private static async Task SendAsync(string subscriptionJson, string payload, string publicKey, string privateKey)
+        {
+            var subscription = JsonConvert.DeserializeObject<Subscription>(subscriptionJson);
+
+            var pushSubscription = new PushSubscription(subscription.ChannelUri, subscription.Keys.P256Dh, subscription.Keys.Auth);
+            var vapidDetails = new VapidDetails("mailto:daniele.scipioni@gmail.com", publicKey, privateKey);
+            await WebPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
+        }
+    }
+}
