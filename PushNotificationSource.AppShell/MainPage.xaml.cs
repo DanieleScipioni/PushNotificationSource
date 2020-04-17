a715b851992bf8b7b817df6c1dbed13ef66346b0
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-04-17 19:31:22 +0200

    Add share

--- PushNotificationSource.AppShell/MainPage.xaml.cs
+++ PushNotificationSource.AppShell/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Threading.Tasks;
 using WebPush;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 
@@ -16,6 +17,14 @@ namespace PushNotificationSource.AppShell
             InitializeComponent();
             PublicKeyTextBox.Text = "BBrspYRJxJQYsqzVp8FuRPqOQ9L6F5vRbIMQu9CeiYGEP68aEVem5b6e1QK2BQYgNIKh4nORAyjKYEZAE7D5SAA";
             PrivateKeyTextBox.Text = "HByTkaPfAXU1TA5eIVqQfLKYAXwRYo388Lx63vXI5OA";
+
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+
+            Unloaded += (sender, args) =>
+            {
+                dataTransferManager.DataRequested -= DataTransferManager_DataRequested;
+            };
         }
 
         private async void ButtonSend_Click(object sender, RoutedEventArgs e)
@@ -62,11 +71,25 @@ namespace PushNotificationSource.AppShell
             await WebPushClient.SendNotificationAsync(pushSubscription, payload, vapidDetails);
         }
 
-        private void CreateKys_OnClick(object sender, RoutedEventArgs e)
+        private void CreateKeys_OnClick(object sender, RoutedEventArgs e)
         {
             VapidDetails keys = VapidHelper.GenerateVapidKeys();
             PrivateKeyTextBox.Text = keys.PrivateKey;
             PublicKeyTextBox.Text = keys.PublicKey;
+        }
+
+        private void Share_OnClick(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+            string value = $"Public: {PublicKeyTextBox.Text}\nPrivate: {PrivateKeyTextBox.Text}";
+            request.Data.SetText(value);
+            request.Data.Properties.Title = "Share keys";
+            request.Data.Properties.Description = "Be carefull, sensitive data!";
         }
     }
 }
